bebdf6c1b4d4aaee37a84be50623f82fbe44a59d
Mario Minondo <mariusminondus@yahoo.com>
2021-07-17 19:01:59 -0600

    Installed Firebase firestore

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyDQ6s4qynUm5IcQ-dgQD-v20SC_JQAsmfM",
+    authDomain: "clone-96e4f.firebaseapp.com",
+    projectId: "clone-96e4f",
+    storageBucket: "clone-96e4f.appspot.com",
+    messagingSenderId: "120313797019",
+    appId: "1:120313797019:web:7dd692462b0199cd58693e",
+    measurementId: "G-RMBSKJDJ33"
+  };
